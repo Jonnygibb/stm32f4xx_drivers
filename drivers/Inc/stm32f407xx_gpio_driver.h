160691c75e969ff3367faf072c93466d5f7e6144
Jonathan Gibbons <jonathan.gibbons6@gmail.com>
2024-08-25 11:06:34 +0100

    Started GPIO_Init function to set registers based on GPIO config

--- drivers/Inc/stm32f407xx_gpio_driver.h
+++ drivers/Inc/stm32f407xx_gpio_driver.h
@@ -15,11 +15,11 @@
  */
 
 typedef struct {
-	uint8_t GPIO_PinNumber;
-	uint8_t GPIO_PinMode;
-	uint8_t GPIO_PinSpeed;
-	uint8_t GPIO_PinPuPdControl;
-	uint8_t GPIO_PinOPType;
+	uint8_t GPIO_PinNumber;				// Possible Pin number from @GPIO_PIN_NOS
+	uint8_t GPIO_PinMode;				// Possible Pin modes from @GPIO_PIN_MODES
+	uint8_t GPIO_PinSpeed;				// Possible Pin Speeds from @GPIO_PIN_SPEEDS
+	uint8_t GPIO_PinPuPdControl;		// Possible Pin pull-up/pull-down config from @GPIO_PIN_PUPD
+	uint8_t GPIO_PinOPType;				// Possible Pin output types from @GPIO_PIN_OP_TYPE
 	uint8_t GPIO_PinAltFunMode;
 } GPIO_PinConfig_t;
 
@@ -32,14 +32,68 @@ typedef struct {
 	GPIO_PinConfig_t GPIO_PinConfig;
 } GPIO_Handle_t;
 
+/*
+ * @GPIO_PIN_NOS
+ * Macro for each of the available pin numbers
+ */
+#define GPIO_PIN_NO_0			0
+#define GPIO_PIN_NO_1			1
+#define GPIO_PIN_NO_2			2
+#define GPIO_PIN_NO_3			3
+#define GPIO_PIN_NO_4			4
+#define GPIO_PIN_NO_5			5
+#define GPIO_PIN_NO_6			6
+#define GPIO_PIN_NO_7			7
+#define GPIO_PIN_NO_8			8
+#define GPIO_PIN_NO_9			9
+#define GPIO_PIN_NO_10			10
+#define GPIO_PIN_NO_11			11
+#define GPIO_PIN_NO_12			12
+#define GPIO_PIN_NO_13			13
+#define GPIO_PIN_NO_14			14
+#define GPIO_PIN_NO_15			15
+
+/*
+ * @GPIO_PIN_MODES
+ * GPIO Pin possible modes
+ */
+
+#define GPIO_MODE_IN			0
+#define GPIO_MODE_OUT			1
+#define GPIO_MODE_ALTFN			2
+#define GPIO_MODE_ANALOG		3
+#define GPIO_MODE_IT_FT			4	// GPIO interrupt mode falling edge
+#define GPIO_MODE_IT_RT			5	// GPIO interrupt mode rising edge
+#define GPIO_MODE_IT_RFT		6	// GPIO interrupt mode rising falling edge
+
+/*
+ * @GPIO_PIN_SPEEDS
+ * GPIO Pin possible output speeds
+ */
+#define GPIO_SPEED_LOW			0
+#define GPIO_SPEED_MEDIUM		1
+#define GPIO_SPEED_HIGH			2
+#define GPIO_SPEED_VERY_HIGH	3
+
+/*
+ * @GPIO_PIN_PUPD
+ * GPIO Pin pull up and pull down states
+ */
+#define GPIO_NO_PUPD			0	// No pull up or pull down
+#define GPIO_PIN_PU				1	// Pull up resistor
+#define GPIO_PIN_PD				2	// Pull down resistor
+
+/*
+ * @GPIO_PIN_OP_TYPE
+ * GPIO Pin possible output types
+ */
+#define GPIO_OP_TYPE_PP			0	// Push-Pull
+#define GPIO_OP_TYPE_OD			1	// Open Drain
+
 
 /********************************************************************************************
  * 								APIs supported by this driver
  *******************************************************************************************/
-
-/*
- * Peripheral Clock Setup
- */
 
 /******************************************************************************
  * GPIO Driver API that enables or disables the Peripheral Clock for the given
@@ -52,9 +106,13 @@ typedef struct {
  ******************************************************************************/
 void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnOrDi);
 
-/*
- * Initialisation and De-Initialisation
- */
+/******************************************************************************
+ * Initialisation function that configures a GPIO Pin based on the parameters
+ * of the GPIOHandle structure.
+ *
+ * @param *pGPIOHandle A pointer a GPIO_Handle_t structure that is contains
+ * 						the desired configuration for the GPIO Pin.
+ ******************************************************************************/
 void GPIO_Init(GPIO_Handle_t *pGPIOHandle);
 void GPIO_DeInit(GPIO_RegDef_t *pGPIOx);
 
--- drivers/src/stm32f407xx_gpio_driver.c
+++ drivers/src/stm32f407xx_gpio_driver.c
@@ -79,6 +79,45 @@ void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnOrDi) {
  */
 void GPIO_Init(GPIO_Handle_t *pGPIOHandle) {
 
+	uint32_t temp = 0;
+
+	// Configure the mode of the GPIO Pin
+	if (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode <= GPIO_MODE_ANALOG) {
+		// Generate the mask for the Pin Mode register. Multiply by 2 since the pin register is 2 bits wide.
+		temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber));
+		pGPIOHandle->pGPIOx->MODER = temp; // Save the value in the GPIO Mode register.
+	} else {
+		//TODO - Configure settings for interrupt modes
+	}
+
+	temp = 0;	// Reset the temp value
+
+	// Configure the Speed of the GPIO pin.
+	// Create another bit shift to place the desired speed in the correct pins register position.
+	// Multiply by 2 again since bit field is 2 bits wide.
+	temp = pGPIOHandle->GPIO_PinConfig.GPIO_PinSpeed << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+	pGPIOHandle->pGPIOx->OSPEEDR = temp;
+
+	temp = 0;
+
+	// Configure the pull-up/pull-down state of the GPIO pin.
+	temp = pGPIOHandle->GPIO_PinConfig.GPIO_PinPuPdControl << (2 * pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+	pGPIOHandle->pGPIOx->PUPDR = temp;
+
+	temp = 0;
+
+	// Configure the output type of the GPIO pin. No need to multiply by 2 since only 1 bit wide register.
+	temp = pGPIOHandle->GPIO_PinConfig.GPIO_PinOPType << (pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber);
+	pGPIOHandle->pGPIOx->OTYPER = temp;
+
+	temp = 0;
+
+	// Configure the alternate functionality registers (if necessary).
+	if (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_ALTFN) {
+		// TODO - Configure Alt function registers.
+	}
+
+
 }
 void GPIO_DeInit(GPIO_RegDef_t *pGPIOx) {
 
