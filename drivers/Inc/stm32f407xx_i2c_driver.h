9e7d9c85ecf1781728a500db72eac8093d261056
Jonathan Gibbons <jonthan.gibbons6@gmail.com>
2025-08-04 23:03:24 +0100

    Added clock calculation functions for I2C freq

--- drivers/Inc/stm32f407xx_i2c_driver.h
+++ drivers/Inc/stm32f407xx_i2c_driver.h
@@ -28,4 +28,79 @@ typedef struct {
     I2C_Config_t I2C_Config;
 } I2C_Handle_t;
 
+/*
+ * @I2C_SCLSpeed 
+ */
+#define I2C_SCL_SPEED_SM        100000
+#define I2C_SCL_SPEED_FM2K      200000
+#define I2C_SCL_SPEED_FM4K      400000
+
+
+/*
+ * @I2C_ACKControl
+ */
+#define I2C_ACK_ENABLE          1
+#define I2C_ACK_DISABLE         0
+
+
+/*
+ * @I2C_FMDutyCycle
+ */
+#define I2C_FM_DUTY_2           0
+#define I2C_FM_DUTY_16_9        1
+
+/********************************************************************************************
+ * 								APIs supported by this driver
+ ********************************************************************************************/
+
+/******************************************************************************
+ * I2C Driver API that enables or disables the Peripheral Clock for the given
+ * I2C interface. ENABLE or DISABLE macro can be used for parameter EnOrDi.
+ *
+ * @param *pI2Cx A pointer to the base address of the I2C interface using the
+ * 					defined register structure.
+ * @param EnOrDi Integer value to enable or disable the peripheral clock. Can
+ * 					be 1 or 0. Alternatively use macro ENABLE or DISABLE.
+ ******************************************************************************/
+ void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi);
+
+ /******************************************************************************
+ * Controls whether the I2C peripheral is on or off.
+ *
+ * @param pI2Cx A pointer to the base address of the I2C interface using the
+ * 				defined register structure.
+ * @param EnOrDi Value to represent whether I2C peripheral will be enabled
+ * 				 or disabled.
+ ******************************************************************************/
+ void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi);
+
+ /******************************************************************************
+  * Initialisation function that configures an I2C interface based on the parameters
+  * of the I2CHandle structure.
+  *
+  * @param *pI2CHandle A pointer to a I2C_Handle_t structure that is contains
+  * 						the desired configuration for the I2C interface.
+  ******************************************************************************/
+ void I2C_Init(I2C_Handle_t *pI2CHandle);
+ 
+ /******************************************************************************
+  * De-initialisation function that sets and clears the reset register for the
+  * I2C interface referenced in pI2Cx.
+  *
+  * @param *pI2Cx A pointer to the base address of the I2C interface using the
+  * 					defined register structure.
+  ******************************************************************************/
+ void I2C_DeInit(I2C_RegDef_t *pI2Cx);
+ 
+ /******************************************************************************
+  * Checks the status of a user defined flag. List of flags can be found in
+  * @FlagName.
+  *
+  * @param *pI2Cx A pointer to the base address of the I2C interface using the
+  * 					defined register structure.
+  * @param FlagName A user defined flag to check the status of.
+  *
+  */
+ uint8_t I2C_GetFlagStatus(I2C_RegDef_t *pI2Cx, uint32_t FlagName);
+
 #endif /* INC_STM32F407XX_I2C`_DRIVER_H_ */
---
+++ drivers/src/stm32f407xx_i2c_driver.c
@@ -0,0 +1,118 @@
+/*
+ * stm32f407xx_I2C_driver.c
+ *
+ *  Created on: Jul 07, 2025
+ *      Author: Jonathan Gibbons
+ */
+
+#include "stm32f407xx.h"
+#include "stm32f407xx_i2c_driver.h"
+
+uint16_t AHB_PreScaler[8] = {2,4,8,16,64,128,256,512};
+uint8_t APB1_PreScaler[4] = {2,4,8,16};
+
+/*
+ * Peripheral Clock Setup
+ */
+void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi) {
+    if(EnOrDi == ENABLE) {
+        if (pI2Cx == I2C1)
+        {
+            I2C1_PCLK_EN();
+        } else if (pI2Cx == I2C2)
+        {
+            I2C2_PCLK_EN();
+        } else if (pI2Cx == I2C3)
+        {
+            I2C3_PCLK_EN();
+        }
+    } else {
+        if (pI2Cx == I2C1)
+        {
+            I2C1_PCLK_DI();
+        } else if (pI2Cx == I2C2)
+        {
+            I2C2_PCLK_DI();
+        } else if (pI2Cx == I2C3)
+        {
+            I2C3_PCLK_DI();
+        }
+    }
+}
+
+/*
+ * I2C Peripheral Control function
+ */
+void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi) {
+    if (EnOrDi == ENABLE) {
+        pI2Cx->CR1 |= (1 << I2C_CR1_PE);
+    } else {
+        pI2Cx->CR1 &= ~(1 << 0);
+    }
+}
+
+uint32_t RCC_GetPLLOutputClock() {
+
+}
+
+/*
+ * Calculates the frequency of the Peripheral clock.
+ */
+uint32_t RCC_GetPCLK1Value(void) {
+	uint32_t pclk1, SystemClk;
+
+	uint8_t clksrc, temp, ahbp, apb1p;
+
+	clksrc = (RCC->CFGR >> 2) & 0x3;
+	
+	if (clksrc == 0) {
+		SystemClk = 16000000;
+	} else if (clksrc == 1) {
+		SystemClk = 8000000;
+	} else if (clksrc == 2) {
+		SystemClk = RCC_GetPLLOutputClock();
+	}
+
+	temp = ((RCC->CFGR >> 4) & 0xF);
+
+	if (temp < 8) {
+		ahbp = 1;
+	} else {
+		AHB_PreScaler[temp-8];
+	}
+
+	temp = ((RCC->CFGR >> 10) & 0x7);
+
+	if (temp < 8) {
+		apb1p = 1;
+	} else {
+		apb1p = APB1_PreScaler[temp-4];
+	}
+
+	pclk1 = (SystemClk / ahbp) / apb1p;
+
+	return pclk1;
+}
+
+
+/*
+ * I2C initialisation function
+ */
+ void I2C_Init(I2C_Handle_t *pI2CHandle) {
+	uint32_t tempreg = 0;
+
+	// Ack Control bit
+	tempreg |= pI2CHandle->I2C_Config.I2C_AckControl << 10;
+	pI2CHandle->pI2Cx->I2C_CR1 = tempreg;
+
+	// Configure the FREQ fields
+	tempreg = 0;
+	tempreg |= RCC_GetPCLK1Value() / 1000000U;
+	pI2CHandle->pI2Cx->I2C_CR2 = (tempreg & 0x3F);
+
+	// Configure the devices own address
+	tempreg |= pI2CHandle->I2C_Config.I2C_DeviceAddress << 1;
+	tempreg |= (1 << 14);
+	pI2CHandle->pI2Cx->I2C_OAR1 = tempreg;
+
+}
